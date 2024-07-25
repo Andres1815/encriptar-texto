b7834df2bdbbd590ecae21df3baa7f21ef6b7aa1
Andres <ajaramillob@unicartagena.edu.co>
2024-07-25 12:58:34 -0500

    Adicion de la funcionalidad para desencriptar un texto

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function encriptar() {
     let dollImage = document.getElementById("muñeco");
   
     
-    let encryptedText = text
+    let encriptar = text
       .replace(/e/gi, "enter")
       .replace(/i/gi, "imes")
       .replace(/a/gi, "ai")
@@ -18,7 +18,7 @@ function encriptar() {
     
     if (text.length !== 0) {
       
-      document.getElementById("texto").value = encryptedText;
+      document.getElementById("texto").value = encriptar;
   
       
       messageTitle.textContent = "Texto encriptado con éxito";
@@ -26,11 +26,53 @@ function encriptar() {
       dollImage.src = "./img/encriptado.jpg";
     } else {
       
-      dollImage.src = "./img/muñeco.png";
-      messageTitle.textContent = "Ningún mensaje fue encontrado";
-      messageParagraph.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+      dollImage.src = "./img/imagenD.jpg";
+      messageTitle.textContent = "Ningún texto fue encontrado";
+      messageParagraph.textContent = "Ingresa el texto que deseas encriptar";
   
       
-      swal("Ooops!", "Debes ingresar un texto", "warning");
+      
+    }
+
+  }
+
+  
+  function desencriptar() {
+    let text = document.getElementById("texto").value;
+    let titleMessage = document.getElementById("titulo-mensaje");
+    let paragraph = document.getElementById("parrafo");
+    let doll = document.getElementById("muñeco");
+  
+    let desencriptar = text
+      .replace(/enter/gi, "e")
+      .replace(/imes/gi, "i")
+      .replace(/ai/gi, "a")
+      .replace(/ober/gi, "o")
+      .replace(/ufat/gi, "u");
+  
+    if (text.length !== 0) {
+      document.getElementById("texto").value = desencriptar;
+      titleMessage.textContent = "Texto desencriptado con éxito";
+      paragraph.textContent = "";
+      doll.src = "./img/desencriptado.jpg";
+    } else {
+      doll.src = "./img/sticker_25.webp";
+      titleMessage.textContent = "Ningún texto fue encontrado";
+      paragraph.textContent = "Ingresa el texto que deseas desencriptar";
+     
     }
   }
+
+  function copiar() {
+    let textArea = document.getElementById("texto");
+    textArea.select();
+    document.execCommand("copy");
+  
+    let titleMessage = document.getElementById("titulo-mensaje");
+    titleMessage.textContent = "Texto copiado con éxito";
+    
+  }
+  
+  
+  
+
